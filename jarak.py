5d6970e984370a5b87ed0e52ce11a1d5e3234c20
Hasan <pertikalmun771@gmail.com>
2026-04-30 16:35:10 +0700

    tambah jarak & suhu

--- jarak.py
+++ jarak.py
@@ -1,2 +1,52 @@
+def konverter_jarak(satuan_awal, nilai_awal, satuan_hasil):
+    data_konversi = {
+        "m" : 1,
+        "km" : 1000,
+        "cm" : 0.01,
+        "mm" : 0.001,
+        "mil" : 1609.34,
+        "yard" : 0.9144,
+        "inci" : 0.0254,
+        "kaki" : 0.3048,
+        "satuan astronomi" : 149597870700, #149.597.870.700
+        "sa" : 149597870700,
+        "light year" :  9460730472580800, #9.460.730.472.580.800
+        "ly" : 9460730472580800
+    }
+
+    if satuan_awal not in data_konversi or satuan_hasil not in data_konversi:
+        return None
+
+    meter = nilai_awal * data_konversi[satuan_awal]
+    nilai_hasil = meter / data_konversi[satuan_hasil]
+
+    return nilai_hasil
 def menu_jarak():
-    print('jarak')
+    while True:
+        print("\n-- Konversi Jarak --")
+        print("Pilihan Satuan")
+        print("Umum      : Mm   | Cm   |  M     | Km")
+        print("Imperial  : Inci | Kaki |  Yard  | Mil")
+        print("Astronomi : Satuan Astronomi (SA)| Light Year (ly)")
+        try:
+            satuan_awal = input("Masukkan Satuan Asal: ").lower()
+            nilai_awal = float(input("Masukkan Nilai: "))
+            satuan_hasil = input("Masukkan Satuan Hasil: ").lower()
+            nilai_hasil = konverter_jarak(satuan_awal, nilai_awal, satuan_hasil)
+            if nilai_hasil == None:
+                print("Satuan tidak valid!")
+            else:
+                print(f"Hasil: {round(nilai_awal, 4)} {satuan_awal} = {round(nilai_hasil, 4)} {satuan_hasil}")
+
+        except ValueError: 
+            print("Input harus berupa angka!")
+
+        print("\n1. Konversi lagi")
+        print("0. Kembali ke menu utama")
+
+        pilihan = input("Pilih: ")
+
+        if pilihan == "0":
+            break
+    
+    
--- suhu.py
+++ suhu.py
@@ -1,2 +1,50 @@
+def konverter_suhu(satuan_awal, nilai_awal, satuan_hasil):
+    
+    if satuan_awal in "Celcius":
+        suhu = nilai_awal
+    elif satuan_awal in "Kelvin":
+        suhu = nilai_awal - 273.15
+    elif satuan_awal in "Fahrenheit":
+        suhu = (nilai_awal - 32) * 5/9
+    elif satuan_awal in "Reamur":
+        suhu = nilai_awal * 5/4
+    else:
+        return None
+
+    if satuan_hasil in "Celcius":
+        nilai_hasil = suhu
+    elif satuan_hasil in "Kelvin":
+        nilai_hasil = suhu + 273.15
+    elif satuan_hasil in "Fahrenheit":
+        nilai_hasil = (suhu * 9/5) + 32
+    elif satuan_hasil in "Reamur":
+        nilai_hasil = suhu * 4/5
+    else:
+        return None
+
+    return nilai_hasil
 def menu_suhu():
-    print('suhu')
+    while True:
+        print("\n-- Konversi Suhu --")
+        print("Pilihan Satuan: Kelvin(K) | Celcius(C) | Fahrenheit(F) | Reamur(R)")
+        
+        try:
+            satuan_awal = input("Masukkan Satuan Asal: ").capitalize()
+            nilai_awal = float(input("Masukkan Nilai: "))
+            satuan_hasil = input("Masukkan Satuan Hasil: ").capitalize()
+            nilai_hasil = konverter_suhu(satuan_awal, nilai_awal, satuan_hasil)
+            if nilai_hasil == None:
+                print("Satuan tidak valid!")
+            else:
+                print(f"Hasil: {nilai_awal} {satuan_awal[0]} = {nilai_hasil} {satuan_hasil[0]}")
+
+        except ValueError: 
+            print("Input harus berupa angka!")
+
+        print("\n1. Konversi lagi")
+        print("0. Kembali ke menu utama")
+
+        pilihan = input("Pilih: ")
+
+        if pilihan == "0":
+            break    
